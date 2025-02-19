2bf99540627c73612fdf415b2d7f8a5dd64ca109
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-19 15:05:05 -0600

    Update Package.swift

--- Tools/Package.swift
+++ Tools/Package.swift
@@ -1,35 +1,5 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-import PackageDescription
-
-let package = Package(
-    name: "GTLR_ServiceGenerator",
-    platforms: [
-        .macOS(.v10_15),
-    ],
-    products: [
-        .executable(
-            name: "ServiceGenerator",
-            targets: ["ServiceGenerator"]
-        ),
-    ],
-    dependencies: [
-        .package(path: "..")
-    ],
-    targets: [
-        .target(
-            name: "ServiceGenerator",
-            dependencies: ["ServiceGeneratorLib"],
-            path: "SwiftPMShim"
-        ),
-        .target(
-            name: "ServiceGeneratorLib",
-            dependencies: [
-                "GoogleAPIClientForRESTCore",
-                "GoogleAPIClientForREST_Discovery",
-            ],
-            path: "ServiceGenerator",
-            publicHeadersPath: "Public"
-        ),
-    ]
-)
+Void
+End
+Delete
+Stop
+Teminate Swift
